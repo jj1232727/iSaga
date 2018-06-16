cb2989ab38a9910430eb6b357e9ce314e09794c1
jj1232727 <julioaidenmorales@gmail.com>
2018-06-16 15:12:45 -0700

    AIO Start

--- iSagaScripts/versionControl.lua
+++ iSagaScripts/versionControl.lua
@@ -2,104 +2,50 @@ Data = {
     Loader = {
         Version = 1.03,
     },
-    Utilities = {
-        evade = {
-            Version = 0,
-        },
-        orbwalker = {
-            Version = 0,
-        },
-        baseult = {
-            Version = 0,
-        },
-        tracker = {
-            Version = 0,
-        },
-    },
-    Dependencies = {
-        commonLib = {
-            Version = 1.09,
-        },
-        callbacks = {
-            Version = 1.01,
-        },
-        changelog = {
-            Version = 1,
-        },
-        menuLoad = {
-            Version = 1.03,
-        },
-        prediction = {
-            Version = 1,
-        },
-    },
+
     Champions = {
-        Sion = {
+        Irelia = {
             Changelog = "Sion Changelog Here",
             Version = 0,
         },
-        Jax = {
+        Jinx = {
             Changelog = "Jax Initial Release",
             Version = 1,
         },
-        Ashe = {
+        Kaisa = {
             Changelog = "Ashe Initial Release",
             Version = 1.02,
         },
-        Ezreal = {
+        Katarina = {
             Changelog = "Ezreal Changelog Here",
             Version = 1.02,
         },
-        Vladimir = {
+        KogMaw = {
             Changelog = "Vladimir Changelog Here",
             Version = 0,
         },
-        Darius = {
+        Orianna = {
             Changelog = "Darius Changelog Here",
             Version = 0,
         },
-        Corki = {
+        Syndra = {
             Changelog = "Corki Changelog Here",
             Version = 0,
         },
-        Twitch = {
+        Viktor = {
             Changelog = "Twitch Initial Release",
             Version = 1,
         },
-        Jhin = {
+        Zed = {
             Changelog = "Jhin Initial Release",
             Version = 1.01,
         },
-        Lucian = {
+        Zoe = {
             Changelog = "Lucian Initial Release",
             Version = 1.03,
         },
-        Draven = {
-            Changelog = "Draven Changelog Here",
-            Version = 1.01,
-        },
     },
-    Core = {
-        Changelog = "Welcome to Project WinRate - The Most Advanced Script Ever on EXT!\
-\
-We work to deliver the best scripting experience possible through our\
-solid developers experience and HQ codebase, where creativity reigns\
-over most limitations brought by an External Platform. \
-\
-Thanks to the seamless implementation of our AutoUpdate system we\
-can ensure you're always up to date without any impact on your game\
-experience!\
-\
-\
-\
-\
-\
-\
-\
-                                 If you want to support us, head to the forums where you can upvote\
-                                 the thread, leave a nice comment or even make a Donation. ",
-        Version = 1,
-    },
+  
 }
 
 return Data
