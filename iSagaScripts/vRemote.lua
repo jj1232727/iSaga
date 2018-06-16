69dac937c0d0c06fad3fe8b27a0bcce7013681b5
jj1232727 <julioaidenmorales@gmail.com>
2018-06-16 15:46:34 -0700

    AIO Start

---
+++ iSagaScripts/vRemote.lua
@@ -0,0 +1,51 @@
+Data = {
+    Loader = {
+        Version = 1.03,
+    },
+
+    Champions = {
+        Irelia = {
+            Changelog = "Sion Changelog Here",
+            Version = 1,
+        },
+        Jinx = {
+            Changelog = "Jax Initial Release",
+            Version = 1,
+        },
+        Kaisa = {
+            Changelog = "Ashe Initial Release",
+            Version = 1.02,
+        },
+        Katarina = {
+            Changelog = "Ezreal Changelog Here",
+            Version = 1.02,
+        },
+        KogMaw = {
+            Changelog = "Vladimir Changelog Here",
+            Version = 0,
+        },
+        Orianna = {
+            Changelog = "Darius Changelog Here",
+            Version = 0,
+        },
+        Syndra = {
+            Changelog = "Corki Changelog Here",
+            Version = 0,
+        },
+        Viktor = {
+            Changelog = "Twitch Initial Release",
+            Version = 1,
+        },
+        Zed = {
+            Changelog = "Jhin Initial Release",
+            Version = 1.01,
+        },
+        Zoe = {
+            Changelog = "Lucian Initial Release",
+            Version = 1.03,
+        },
+    },
+  
+}
+
+return Data
