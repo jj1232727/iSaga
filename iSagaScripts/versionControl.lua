f0406b87f09d812bcad1922514aed4137058b8ae
jj1232727 <julioaidenmorales@gmail.com>
2018-06-16 14:30:18 -0700

    AIO Start

---
+++ iSagaScripts/versionControl.lua
@@ -0,0 +1,105 @@
+Data = {
+    Loader = {
+        Version = 1.03,
+    },
+    Utilities = {
+        evade = {
+            Version = 0,
+        },
+        orbwalker = {
+            Version = 0,
+        },
+        baseult = {
+            Version = 0,
+        },
+        tracker = {
+            Version = 0,
+        },
+    },
+    Dependencies = {
+        commonLib = {
+            Version = 1.09,
+        },
+        callbacks = {
+            Version = 1.01,
+        },
+        changelog = {
+            Version = 1,
+        },
+        menuLoad = {
+            Version = 1.03,
+        },
+        prediction = {
+            Version = 1,
+        },
+    },
+    Champions = {
+        Sion = {
+            Changelog = "Sion Changelog Here",
+            Version = 0,
+        },
+        Jax = {
+            Changelog = "Jax Initial Release",
+            Version = 1,
+        },
+        Ashe = {
+            Changelog = "Ashe Initial Release",
+            Version = 1.02,
+        },
+        Ezreal = {
+            Changelog = "Ezreal Changelog Here",
+            Version = 1.02,
+        },
+        Vladimir = {
+            Changelog = "Vladimir Changelog Here",
+            Version = 0,
+        },
+        Darius = {
+            Changelog = "Darius Changelog Here",
+            Version = 0,
+        },
+        Corki = {
+            Changelog = "Corki Changelog Here",
+            Version = 0,
+        },
+        Twitch = {
+            Changelog = "Twitch Initial Release",
+            Version = 1,
+        },
+        Jhin = {
+            Changelog = "Jhin Initial Release",
+            Version = 1.01,
+        },
+        Lucian = {
+            Changelog = "Lucian Initial Release",
+            Version = 1.03,
+        },
+        Draven = {
+            Changelog = "Draven Changelog Here",
+            Version = 1.01,
+        },
+    },
+    Core = {
+        Changelog = "Welcome to Project WinRate - The Most Advanced Script Ever on EXT!\
+\
+We work to deliver the best scripting experience possible through our\
+solid developers experience and HQ codebase, where creativity reigns\
+over most limitations brought by an External Platform. \
+\
+Thanks to the seamless implementation of our AutoUpdate system we\
+can ensure you're always up to date without any impact on your game\
+experience!\
+\
+\
+\
+\
+\
+\
+\
+                                 If you want to support us, head to the forums where you can upvote\
+                                 the thread, leave a nice comment or even make a Donation. ",
+        Version = 1,
+    },
+}
+
+return Data
---
+++ iSagaScripts/versionControl2.lua
@@ -0,0 +1,118 @@
+Data = {
+    Champions = {
+        Ashe = {
+            Version = 1.02,
+            Changelog = "Ashe Initial Release",
+        },
+        Corki = {
+            Version = 1.04,
+            Changelog = "OnDash Typo Fix",
+        },
+        Darius = {
+            Version = 1.02,
+            Changelog = "Darius Initial Release",
+        },
+        Draven = {
+            Version = 1.02,
+            Changelog = "Draven Initial Release",
+        },
+        Ezreal = {
+            Version = 1.02,
+            Changelog = "Ezreal LastHit Fix",
+        },
+        Jax = {
+            Version = 1.00,
+            Changelog = "Jax Initial Release",
+        },
+        Lucian = {
+            Version = 1.03,
+            Changelog = "Lucian Initial Release",
+        },
+        Jhin = {
+            Version = 1.01,
+            Changelog = "Jhin Initial Release",
+        },
+        Riven = {
+            Version = 1.01,
+            Changelog = "Riven Initial Release",
+        },
+        Sion = {
+            Version = 1.02,
+            Changelog = "Sion Initial Release",
+        },
+        Syndra = {
+            Version = 1.01,
+            Changelog = "Syndra Initial Release",
+        },
+        TwistedFate = {
+            Version = 1.00,
+            Changelog = "TF Initial Release",
+        },
+        Twitch = {
+            Version = 1.02,
+            Changelog = "Combo/Harass E Fixed \n Ult Can Now Be Disabled",
+        },
+        Vayne = {
+            Version = 1.00,
+            Changelog = "Vayne Initial Release",
+        },
+        Vladimir = {
+            Version = 1.05,
+            Changelog = "Vladimir Initial Release",
+        },
+        Xayah = {
+            Version = 1.00,
+            Changelog = "Xayah Initial Release",
+        },
+    },
+    Loader = {
+        Version = 1.03,
+    },
+    Dependencies = {
+        commonLib = {
+            Version = 1.09,
+        },
+        prediction = {
+            Version = 1,
+        },
+        changelog = {
+            Version = 1.00,
+        },
+        callbacks = {
+            Version = 1.01,
+        },
+        menuLoad = {
+            Version = 1.03,
+        },
+    },
+    Utilities = {
+        baseult = {
+            Version = 0,
+        },
+        evade = {
+            Version = 0,
+        },
+        tracker = {
+            Version = 0,
+        },
+        orbwalker = {
+            Version = 0,
+        },
+    },
+    Core = {
+        Version = 1.00,
+        Changelog = 
+            "Welcome to Project WinRate - The Most Advanced Script Ever on EXT!\n\n"..
+            "We work to deliver the best scripting experience possible through our\n"..
+            "solid developers experience and HQ codebase, where creativity reigns\n" ..
+            "over most limitations brought by an External Platform. \n\n" ..
+            "Thanks to the seamless implementation of our AutoUpdate system we\n"..
+            "can ensure you're always up to date without any impact on your game\n"..
+            "experience!\n\n\n\n\n\n\n\n"..
+            "                                 If you want to support us, head to the forums where you can upvote\n"..
+            "                                 the thread, leave a nice comment or even make a Donation. "            
+        ,
+    },
+}
+
+return Data
